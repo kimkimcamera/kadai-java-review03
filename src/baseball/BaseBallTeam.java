8799faadd4370a5a3964d25154b70f190341c862
kimkimcamera <kimkimcamera@gmail.com>
2023-12-10 15:10:40 +0900

    オブジェクト指向プログラミングの課題です。

---
+++ src/baseball/BaseBallTeam.java
@@ -0,0 +1,61 @@
+package baseball;
+
+public class BaseBallTeam {
+    
+    private String name;
+    private int win;
+    private int lose;
+    private int draw;
+    
+    public BaseBallTeam() {
+    }
+    
+    public BaseBallTeam(String name, int win, int lose, int draw) {
+        this.name = name;
+        this.win = win;
+        this.lose = lose;
+        this.draw = draw;
+    }
+    
+    public double getRate() {
+        double rate = (double) win / (win + lose);
+        return rate;
+    }
+    
+    public void report() {
+        System.out.println( this.name + " の2022年の成績は " + this.win + "勝 " + this.lose + "敗 " + this.draw + "分、勝率は " + getRate() + "です。 ");
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public void setName(String name) {
+    this.name = name;
+}
+    
+    public int getWin() {
+        return this.win;
+    }
+    
+    public void setWin(int win) {
+        this.win = win;
+    }
+    
+    public int getLose() {
+        return this.lose;
+    }
+    
+    public void setLose(int lose) {
+        this.lose = lose;
+    }
+    
+    public int getDraw() {
+        return this.draw;
+    }
+    
+    public void setDraw(int draw) {
+        this.draw = draw;
+    }
+
+}
